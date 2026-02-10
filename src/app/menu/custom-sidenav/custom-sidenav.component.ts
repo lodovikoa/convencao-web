eb416a2a2f25e8fe4892d8efb7c9344fe64dd7fc
lodovikoa <lodoviko@gmail.com>
2026-02-10 09:43:29 -0300

    feat: 20260210 - Construção layout.. menus

--- src/app/menu/custom-sidenav/custom-sidenav.component.ts
+++ src/app/menu/custom-sidenav/custom-sidenav.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { MenuItem } from '../interface/menu-item';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-custom-sidenav',
-  imports: [],
+  imports: [CommonModule, MatListModule, MatIconModule, RouterModule],
   templateUrl: './custom-sidenav.component.html',
   styleUrl: './custom-sidenav.component.scss',
 })
 export class CustomSidenavComponent {
 
+  menuItems = signal<MenuItem[]>([
+    {
+      icon: 'dashboard',
+      label: 'Dashboard',
+      route: 'dashboard'
+    },
+    {
+      icon: 'video_library',
+      label: 'Content',
+      route: 'content'
+    },
+    {
+      icon: 'people',
+      label: 'Users',
+      route: 'users'
+    }
+  ]);
 }
