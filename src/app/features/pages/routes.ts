397fbfcbb7ec60651b89540454a45528167cfafa
lodovikoa <lodoviko@gmail.com>
2026-03-15 09:02:04 -0300

    refactor: 20260315 - renomeado o folder de estados para estado

--- src/app/features/pages/routes.ts
+++ src/app/features/pages/routes.ts
@@ -7,7 +7,7 @@ import { VideosComponent } from "./content/videos/videos.component";
 import { PlaylistsComponent } from "./content/playlists/playlists.component";
 import { PostsComponent } from "./content/posts/posts.component";
 import { HomeComponent } from "./home/home.component";
-import { EstadoComponent } from "./configuracao/estados/listar/estado.component";
+import { EstadoComponent } from "./configuracao/estado/listar/estado.component";
 import { ConfiguracaoComponent } from "./configuracao/configuracao.component";
 
 export default [
